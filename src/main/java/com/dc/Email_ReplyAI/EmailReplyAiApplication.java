3120a6c03974cedc60033f51b055ecd6163a0526
Divyansh Chawla <divyanshchawla23@gmail.com>
2025-06-05 16:02:58 +0530

    Boilerplate code

---
+++ src/main/java/com/dc/Email_ReplyAI/EmailReplyAiApplication.java
@@ -0,0 +1,13 @@
+package com.dc.Email_ReplyAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailReplyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailReplyAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dc/Email_ReplyAI/EmailReplyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.Email_ReplyAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailReplyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
